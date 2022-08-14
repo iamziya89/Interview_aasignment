dd563baa8416bd9e330aafd725fbb587911b277c
Mohd Ziya <iamziya53@gmail.com>
2022-08-14 15:46:35 +0530

    Interview Assignment

--- app.js
+++ app.js
@@ -19,52 +19,15 @@ function myFunction() {
         <button id="btn5" class="size1"></button>
         <button id="btn6" class="size1"></button>
     </div>`
-// =========================================================================
-    // for cancel 
+
+
     let cancel = document.getElementById("cancel");
     cancel.addEventListener("click",function(){
         box.innerHTML = "";
     })
     
 // ===========================================================================
-    // for btn2 add event listner
-    let btn2 = document.getElementById("btn2");
-    btn2.addEventListener("click",function(){
-        let newdiv = document.createElement("div");
-        newdiv.setAttribute("class","box")
-        newdiv.setAttribute("id",`box${count}`)
-        newdiv.innerHTML = ` <div class="color"></div>
-        <div class="write"></div>
-        <div class="font">
-            <span><i class="fa-solid fa-check"></i></span>
-            <span><i class="fa-solid fa-trash"></i></span>
-            <span><i class="fa-solid fa-pen"></i></span>
-            <span id="lock"><i class="fa-solid fa-lock"></i></span>
-        </div>`
-       
-        itemcontainer.appendChild(newdiv);
-
-// For color picker============================================================
-         
-        let color = document.querySelector(".color");
-        console.log(color)
-        let red = document.querySelectorAll(".size1");
-        console.log(red)
-        color.style.background = "red"
-
-// For input box value.............................................................. 
-
-        let write = document.querySelectorAll(".write")[count-1];
-        write.setAttribute("id",`box${count}`);
-        let writebox = document.querySelectorAll(`#box${count}`)[1];
-        writebox.innerHTML = item.value;
-        count += 1;
-        box.innerHTML = ""
-        body.style.background = "white"
-
-    })
-   
-}
+    
 
 
 
